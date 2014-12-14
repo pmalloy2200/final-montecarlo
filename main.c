9fe413f1c8ac1df7fdf46df387ce30c812e55696
pmalloy2200 <patrick.malloy@uconn.edu>
2014-12-14 04:29:15 -0500

    fixed .gitignore, processed for readability

--- main.c
+++ main.c
@@ -6,38 +6,41 @@
 
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_statistics.h>
-     
-/* mc_pi.c */
-double mc_se (long m, int n, gsl_rng *r);
 
-#define POINTS 500000    /* number of random points to generate */
-#define NEXP 50           /* number of experiments */
+double mc_se (long m, int n, gsl_rng * r);
 
-int main(void) {
-    double vol[NEXP];      /* calculated values of volume */
+#define POINTS 500000           /* number of random points to generate */
+#define NEXP 50                 /* number of experiments */
+
+int
+main (void)
+{
+    double vol[NEXP];           /* calculated values of volume */
     gsl_rng *r;
     double mean, sd;
     int n, j;
     unsigned long seed = 1UL;
     long p = POINTS;
-     
+
     /* allocate random number generator */
-    r = gsl_rng_alloc(gsl_rng_taus2);
+    r = gsl_rng_alloc (gsl_rng_taus2);
 
     /* seed the random number generator */
-    gsl_rng_set(r, seed);
+    gsl_rng_set (r, seed);
 
-    for (n = 1; n < 10; n++) {
-        for (j = 0; j < NEXP; j++) {
+    for (n = 1; n < 10; n++)
+    {
+        for (j = 0; j < NEXP; j++)
+        {
             /* calculate pi using Monte Carlo algorithm */
-            vol[j] = mc_se(p, n, r);
+            vol[j] = mc_se (p, n, r);
         }
-        mean = gsl_stats_mean(vol, 1, NEXP);
-        sd = gsl_stats_sd_m(vol, 1, NEXP, mean);
-        printf("%15d   %10.8f   %10.8f\n", n, mean, sd);
+        mean = gsl_stats_mean (vol, 1, NEXP);
+        sd = gsl_stats_sd_m (vol, 1, NEXP, mean);
+        printf ("%15d   %10.8f   %10.8f\n", n, mean, sd);
     }
 
-    gsl_rng_free(r);
+    gsl_rng_free (r);
 
-    return(0);
+    return (0);
 }
--- mc_se.c
+++ mc_se.c
@@ -5,26 +5,31 @@
 #include <gsl/gsl_rng.h>
 #include <math.h>
 
-double mc_se(long m, int n, gsl_rng *r)
+double
+mc_se (long m, int n, gsl_rng * r)
 {
-   double x, y, a, b, c;       /* 5-dim coordinate, all between 0 and 1  */
-   long count = 0L;
-   long i;
-   for (i = 0; i < m; i++) {
-      /* generate random coordinates */
-      x = gsl_rng_uniform(r);
-      y = gsl_rng_uniform(r);
-      a = gsl_rng_uniform(r);
-      b = gsl_rng_uniform(r);
-      c = gsl_rng_uniform(r);
+    double x, y, a, b, c;       /* 5-dim coordinate, all between 0 and 1  */
+    long count = 0L;
+    long i;
+    for (i = 0; i < m; i++)
+    {
+        /* generate random coordinates */
+        x = gsl_rng_uniform (r);
+        y = gsl_rng_uniform (r);
+        a = gsl_rng_uniform (r);
+        b = gsl_rng_uniform (r);
+        c = gsl_rng_uniform (r);
 
-      /* count when the point lands in the shape */
-      if ((pow(x, (double) n) + pow(y, (double) n) + 
-      pow(a, (double) n) + pow(b, (double) n) + pow(c, (double) n)) <= 1.0) {
-         count += 1;
-      }
-   }
+        /* count when the point lands in the shape */
+        if ((pow (x, (double) n) + pow (y, (double) n) +
+             pow (a, (double) n) + pow (b, (double) n) + pow (c,
+                                                              (double) n)) <=
+            1.0)
+        {
+            count += 1;
+        }
+    }
 
-   return(32.0 * ((double)count)/((double)m));
+    return (32.0 * ((double) count) / ((double) m));
 
-} 
+}
